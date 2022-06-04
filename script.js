e60932df354ed6bdd1753e849517091b47614ce4
chinh <danhchinh2024@gmail.com>
2022-06-04 17:51:33 +0700

    lan commit thu 3

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 
 
-// var black_open = document.querySelectorAll(".black");
-// var red_open = document.querySelectorAll(".red");
+
 
 function getValuaeName(childCell,name){
     return childCell.attributes[`${name}`].value
 }
 
-// var text = getValuaeName(black_open[0],"name");
-// console.log(text)
-
-// function createStepOfChariot(row,col){
-// }
+//return chessman on chessboard
+function browseTheArray(Array2D){
+    for( i in Array2D){
+        for(j in Array2D[i]){
+            if (Array2D[i][j].classList.contains("chessman")){
+                //dosomething
+            }
+        }
+    }
+}
 
 function makeChessboard() {
     var cells = document.querySelectorAll(".cell");
@@ -23,6 +27,9 @@ function makeChessboard() {
             var cell = cells[count];
             cell.setAttribute("row", i);
             cell.setAttribute("col", j);
+            if(cell.classList.contains("chessman")){
+                cell.innerText = getValuaeName(cell,"name");
+            }
             row.push(cell);
             count++;
         }
@@ -31,32 +38,70 @@ function makeChessboard() {
     return chessboard;
 
 }
-function makeChessmans(color){
-    var chessmans = [];
 
-    var start = 0;
-    var end = 5;
-    if(color!="black"){
+function makeNextStep(color){
+    var start = 0, end = 5;
+    if(color == "red"){
         start = 5;
         end = 10;
     }
-
-    for (let i = start; i < end; i++) {
-        for (let j = 0; j < 9; j++) {
-            if(getValuaeName(chessboard[i][j],"name")!="null"){
-                chessmans.push(chessboard[i][j])
-                ////////////////
-                chessboard[i][j].classList.add("green");
-                chessboard[i][j].innerText = getValuaeName(chessboard[i][j],"name");
-                chessboard[i][j].style.color = "white";
-                ////////////////
+    for(let i=start; i<end; i++){
+        for(let j=0; j<9; j++){
+            var cell = chessboard[i][j];
+            if(cell.classList.contains("chessman")){
+                doSomething(cell);
             }
         }
     }
-    return chessmans
+}
+
+//return index_list
+function doSomething(chessman){
+    // console.log(chessman)
+
+    var chessname = getValuaeName(chessman,"name");
+    var row = getValuaeName(chessman,"row");
+    var col = getValuaeName(chessman,"col");
+    // console.log(`[${chessname}]: ${row}:${col}`);
+    // switch(chessname) {
+    //     case "soldier":
+    //         // console.log(getValuaeName(chessman,"row"),getValuaeName(chessman,"col"));
+    //       // code block
+    //       break;
+    //     case "":
+    //       // code block
+    //       break;
+    //     default:
+    //       // code block
+    //   }
+
+    return [[0,1]]; // index on chessboard
+}
+
+function changeChessboard(row,col,newrow,newcol){
+    var tmp = chessboard[row][col];
+    chessboard[newrow][newcol].innerText = chessboard[row][col].innerText;
+    chessboard[newrow][newcol].setAttribute("name",getValuaeName(chessboard[row][col],"name"));
+    chessboard[newrow][newcol].classList.add("chessman");
+    chessboard[row][col].innerText = "";
+    chessboard[row][col].removeAttribute("name");
+    chessboard[row][col].classList.remove("chessman");
+
+}
+function moveByClick(){
+    for( i in chessboard){
+        for(j in chessboard[i]){
+            if (chessboard[i][j].classList.contains("chessman")){
+                chessboard[i][j].onclick = ()=>{
+                    console.log(chessboard[i][j]);
+                    // chessboard[i][j].classList.// bật tắt click
+                }
+            }
+        }
+    }
+
 }
 var chessboard = makeChessboard();
-var black_chessmans = makeChessmans("black");
-console.log(black_chessmans);
-var red_chessmans = makeChessmans("red");
-console.log(red_chessmans);
+// makeNextStep("red");
+
+moveByClick(chessboard);
